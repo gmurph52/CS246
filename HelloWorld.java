336876a4e8077cc648061c313dc13be2d521d71a
gmurph52 <bobsmith52bananas@yahoo.com>
2015-05-13 19:03:02 -0600

    This is my hello get hub world file going to test branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,6 +15,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello GitHub World");
+        System.out.println("testing...");
     }
     
 }
