bedb31aa5cc6b828fdcb6a283eeefe9378e04d5f
gmurph52 <bobsmith52bananas@yahoo.com>
2015-05-13 18:39:07 -0600

    This is my hello world file

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Hello World
+ */
+package helloworld;
+
+/**
+ * CLASS HELLO WORLD
+ * 
+ * @author Gerald
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
